b9d8592dd188cdcf283adab7fe9246830dd04155
Ansh Patel <85411358+anshptl21@users.noreply.github.com>
2022-09-10 17:30:32 -0500

    Add files via upload

--- wordle.js
+++ wordle.js
@@ -1,6 +1,8 @@
 var keyboard = [];
 var letterList = [];
-var repeatingList = [];
+var row = [];
+var id = 1;
+var answer = ["A", "S", "S", "E", "T"];
 function creatingKeyBoard(){
 
 	/* creating array list for keyboard */
@@ -98,7 +100,7 @@ function choosingKeyBoard(key) {
 		id++;
 	}
 
-}
+} 
 
 
 document.addEventListener("keypress", function(event) {
@@ -121,7 +123,6 @@ document.addEventListener("keydown", function(event){
 
 });
 
-var id = 1;
 function enterWord(){
 	var i = 0;
 	var t = 0;
@@ -130,49 +131,44 @@ function enterWord(){
 		var text_box_value = document.getElementById(id).value;
 		if (document.getElementById(5).value !== undefined){
 			while (id <= 5){
-				checkRepeatedLetters(i, t, id);
+				color(id, i)
+				showWin(text_box)
 				id++;
-				t++;
 				i++;
 			}
-			document.getElementsByClassName("row1 one").value = "enter";
+			document.getElementsByClassName("row1 one").value = "enter"
 		}
 		if (document.getElementById(10).value !== undefined){
-			letterList = [];
-			i = 0;
-			t = 0;
+			row = [];
+			answer = ["L", "O", "T", "U", "S"];
 			while (id <= 10){
-				checkRepeatedLetters(i, t, id);
+				color(id, i)
+				showWin(text_box)
 				id++;
-				t++;
 				i++;
 			}
 			document.getElementsByClassName("row2 one").value = "enter";
 
 		}
 		if (document.getElementById(15).value !== undefined){
-			letterList = [];
-			i = 0;
-			t = 0;
+			row = [];
+			answer = ["L", "O", "T", "U", "S"];
 			while (id <= 15){
-				checkRepeatedLetters(i, t, id);
-				className_value = "enter"
+				color(id, i)
+				showWin(text_box)
 				id++;
-				t++;
 				i++;
 
 			}
 			document.getElementsByClassName("row3 one").value = "enter";
 		}
 		if (document.getElementById(20).value !== undefined){
-			letterList = [];
-			i = 0;
-			t = 0;
+			row = [];
+			answer = ["L", "O", "T", "U", "S"];
 			while (id <= 20){
-				checkRepeatedLetters(i, t, id);
-				className_value = "enter"
+				color(id, i)
+				showWin(text_box)
 				id++;
-				t++;
 				i++;
 
 			}
@@ -181,14 +177,13 @@ function enterWord(){
 
 		}
 		if (document.getElementById(25).value !== undefined){
-			letterList = [];
-			i = 0;
-			t = 0;
+			row = [];
+			answer = ["L", "O", "T", "U", "S"];
 			while (id <= 25){
-				checkRepeatedLetters(i, t, id);
-				className_value = "enter"
+				color(id, i)
+				showWin(text_box)
+				showAnswer(id)
 				id++;
-				t++;
 				i++;
 
 			}
@@ -197,81 +192,6 @@ function enterWord(){
 }
 
  
-function checkRepeatedLetters(i, t, id){
-	var ans = "ASSET";
-	var text_box = document.getElementById(id).className;
-		if (text_box === "row1 one"){
-				if (document.getElementById(id).value !== undefined){
-					letterList.push(document.getElementById(id).innerHTML);
-				}
-		} else if (text_box === "row2 one"){
-				if (document.getElementById(id).value !== undefined){
-					letterList.push(document.getElementById(id).innerHTML);
-				}
-		} else if (text_box === "row3 one"){
-				if (document.getElementById(id).value !== undefined){
-					letterList.push(document.getElementById(id).innerHTML);
-				}
-		} else if (text_box === "row4 one"){
-				if (document.getElementById(id).value !== undefined){
-					letterList.push(document.getElementById(id).innerHTML);
-				}
-		} else if (text_box === "row5 one"){
-			if (document.getElementById(id).value !== undefined){
-				letterList.push(document.getElementById(id).innerHTML);
-			}
-		}
-		while(t <= 4 && i <= 4){
-			if (letterList[i] === ans[t]){
-				document.getElementById(id).style.background = "#29a329";
-				document.getElementById(id).style.border = "3px solid transparent"
-				break;
-			} else if (ans.includes(letterList[i])){
-				document.getElementById(id).style.background = "#e6e600";
-				duplicateCharCount(ans, t, id);
-				/*var match = /[S]+/g;
-				var letter = letterList[i];
-				console.log(letter.match(match));*/
-				document.getElementById(id).style.border = "3px solid transparent"
-				break;
-			} else {
-		 		document.getElementById(id).style.background = "#476b6b";
-		 		console.log(document.getElementById(id).style.background)
-		 		document.getElementById(id).style.border = "3px solid transparent"	
-		 		break;
-			}
-		}
-		showWin(text_box)
-		showAnswer(ans, id)
-
-	}
-
-
-
-
-
-function duplicateCharCount(word, id) {
-	var repeatingList = [];
-	var inputList = [];
-	var timesList = [];
-
-	if(word) {
-		var obj = {};
-		for(let i = 0; i < word.length; i++) {
-			if(obj[word[i]]){
-				obj[word[i]] += obj[word[i]];
-				repeatingList.push(word[i]);
-				timesList.push(obj[word[i]]);
-			}else {
-				obj[word[i]] = 1;
-			}
-		}
-		console.log(repeatingList);
-		console.log(timesList);
-
-	}
-	
-}
 
 
 
@@ -283,21 +203,20 @@ function showWin(classname){
 			if (document.getElementById(letter).style.background === "rgb(41, 163, 41)"){
 				counter++;
 		}
+			if (counter === 6){
+				document.getElementById("win").innerHTML = "GREAT"
+				document.getElementById("win").style.display = "inherit"
+				document.getElementById("win").style.animation = "1s ease-in fadeIn"
+			}
 	}
 	letter++;
 }
-	if (counter === 6){
-		$("#win").css("display", "inherit");
-		document.getElementById("win").innerHTML = "AMAZING"
-		$("#win").addClass("animated fadeIn")
-	} else {
-		return false;
-	}
-}
 
+}
 var backspace = document.getElementById("backspace");
 
 backspace.onclick = function(){
+
 	var id = 1;
 	if (document.getElementsByClassName("row1 one").value === "enter"){
 		id = 7;
@@ -310,6 +229,9 @@ backspace.onclick = function(){
 	}
 	if (document.getElementsByClassName("row4 one").value === "enter"){
 		id = 22;
+	}
+	if (document.getElementById("win").style.display === "inherit"){
+		id = 27;
 	}
 	while (id <= 25){
 	if (document.getElementById(id).value === undefined){
@@ -348,12 +270,15 @@ function backSpace(){
 	}	
 }
 
-function showAnswer(word, id){
-	if(id == 25){
-		if (document.getElementById("win").innerHTML !== "AMAZING"){
+function showAnswer(id){
+	word = "LOTUS"
+	if(id >= 25){
+		console.log("showAnswer")
+		if (document.getElementById("win").innerHTML !== "GREAT"){
+			console.log("SHOW")
 			$("#win").css("display", "inherit");
 			document.getElementById("win").innerHTML = word
-			$("#win").addClass("animated fadeIn")
+			document.getElementById("win").style.animation = "1s ease-in fadeIn"
 
 		} else {
 			return false
@@ -361,4 +286,33 @@ function showAnswer(word, id){
 	} 
 }
 
+
+
+
+function color(id, i){
+	row.push(document.getElementById(id).innerHTML);
+	while (i < row.length){
+		console.log(row)
+		console.log(answer)
+		console.log("I before " + i)
+		if (row[i] === answer[i]){
+			document.getElementById(id).style.background = "#29a329";
+			document.getElementById(id).style.border = "3px solid transparent";
+			answer[i] = 0;
+			break;
+		} else if(answer.includes(row[i])){
+			document.getElementById(id).style.background = "rgb(230, 230, 0)";
+			document.getElementById(id).style.border = "3px solid transparent"
+			index = answer.indexOf(row[i]);
+			answer[index] = 0;
+			break;
+			} else {
+				document.getElementById(id).style.background = "#476b6b";
+				document.getElementById(id).style.border = "3px solid transparent"
+				break;
+			}
+			
+		}
+	}
+	
 
